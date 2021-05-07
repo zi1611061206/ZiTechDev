704311b0eafc537b11913f507c327f8e0e2cbb05
zi1611061206 <sotayvaodoi@gmail.com>
2021-05-07 16:04:44 +0700

    Create Business Structure And Complete Activity Business

---
+++ ZiTechDev.Business/Engines/Paginition/PaginitionEngines.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZiTechDev.Business.Engines.Paginition
+{
+    public class PaginitionEngines<T>
+    {
+        public List<T> Item { get; set; }
+
+        public int TotalRecord { get; set; }
+    }
+}
---
+++ ZiTechDev.Business/Requests/Activity/ActivityFilter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ZiTechDev.Business.Engines.Paginition;
+
+namespace ZiTechDev.Business.Requests.Activity
+{
+    public class ActivityFilter : PaginitionConfiguration
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<int> FunctionIds { get; set; }
+
+        public ActivityFilter()
+        {
+            Id = 0;
+            Name = null;
+            FunctionIds = null;
+        }
+    }
+}
